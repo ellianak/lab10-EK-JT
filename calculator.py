76a756d4678ba1589a779483ac0699203fc4318a
Elliana Kirby <elliana.kirby@ufl.edu>
2025-04-14 15:29:52 -0400

    Update calculator.py
    Updated calculator file

--- calculator.py
+++ calculator.py
@@ -4,8 +4,28 @@ calculator.py
 
 One function per operation, in order.
 """
-# First example
-def add(a, b): 
-    pass
+import math
+def square_root(a):
+    if a < 0:
+        raise ValueError
+    return math.sqrt(a)
+def hypotenuse(a, b):
+    return math.hypot(a, b)
 
-
+def add(a, b):
+    return a+b
+def subtract(a, b):
+    return a-b
+def mul(a, b):
+    return a*b
+def div(a, b):
+    if a == 0:
+        raise ZeroDivisionError
+    else:
+        return b/a
+def logarithm(a, b):
+    if b <= 0:
+        raise ValueError
+    math.log(a, b)
+def exp(a, b):
+    return a**b
